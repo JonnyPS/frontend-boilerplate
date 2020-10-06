5b34f2f7a70ed96c2f75cef4fed139a425b48fdc
Jonny Spry <j.spry@arts.ac.uk>
2020-10-06 20:06:17 +0100

    add moment node package for date formatting

--- src/components/insta-feed-summary.js
+++ src/components/insta-feed-summary.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 export class InstaFeedSummary extends Component {
   render() {
 
     console.log('props', this.props.dataStream.instagramData)
 
-    let formatDate = ((data) => {
-      return data.toDateString()
-    })
+    // let formatDate = ((data) => {
+    //   console.log(typeof data)
+    //    let day = moment(data);
+    //   //  return day
+    // })
 
     return (
       <div className="grid-container-25 instagram-feed-container">
@@ -15,7 +19,7 @@ export class InstaFeedSummary extends Component {
           {this.props.dataStream.instagramData.map((item, index) => {
             return <div key={index}>
               <span>Account: {item.college}</span>
-              <span>Last Updated At: {item.last_updated}</span>
+              <span>Last Updated At: {<Moment format="Do MMMM YYYY HH:MMa">{item.last_updated}</Moment>}</span>
               <span>Status: {item.status === "ok" ? <span className="status-ok"></span> : <span className="status-error"></span>}</span>
             </div>
           })}
